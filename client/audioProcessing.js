dbd57c2bc064956a6d99f230c8df734893f0fc0f
pfoo06 <jyst48@durham.ac.uk>
2026-04-22 14:38:55 +0100

    get-audio-stream

--- client/audioProcessing.js
+++ client/audioProcessing.js
@@ -1,6 +1,11 @@
 class AudioProcessor extends AudioWorkletProcessor {
     process(inputs, outputs, parameters) {
 
+        const input = inputs[0];
+        if(input && input.length > 0){
+            const channeldata = input[0];
+            this.port.postMessage(channeldata);
+        }
         return true;
     }
 }
