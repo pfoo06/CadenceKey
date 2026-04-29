4f84483b72f390933fda933eddfbb635a9818b4e
Anne-Mei Ong <annemeiong2@gmail.com>
2026-04-29 10:01:59 +0100

    added instructions and info

--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
 
 
-const views = {landing : document.getElementById('landing-view'), gallery : document.getElementById('gallery-view')};
+const views = {landing : document.getElementById('landing-view'), gallery : document.getElementById('gallery-view'), info : document.getElementById('info-view')};
+
 const entryBtn = document.getElementById('entryBtn');
-
-
 const  cadenceKeyLogo = document.getElementById('homeLogo');
 const infoLink = document.getElementById('infoLink');
+
 function switchView (viewID) {
     Object.values(views).forEach(div => {div.classList.add('hidden')});
     views[viewID].classList.remove('hidden');
